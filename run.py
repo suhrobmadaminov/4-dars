a335f2d052c98180fb46f3d58b3a8ae01475e675
Suhrob Madaminov <suhrobmadaminov12@gmail.com>
2025-08-21 13:01:46 +0500

    kod yangilandi

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ class Reg(StatesGroup):
     number = State()
 
 # 1-Handler - Start komandasi
+print("Bu yangi commit uchun")
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(f"Assalomu alaykum hurmatli foydalanuvchi!\nTest Botimizga xush kelibsiz!", 
